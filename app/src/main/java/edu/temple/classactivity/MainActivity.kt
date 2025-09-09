5215fecf0ab17c6a7558ece169ff493571977459
CarsonU <carson.umberger@gmail.com>
2025-09-09 15:00:38 -0400

    add button and text field functionality

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,6 +1,10 @@
 package edu.temple.classactivity
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +14,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            var name = findViewById<EditText>(R.id.editText).text
+            if (name.isNullOrBlank()){
+                findViewById<TextView>(R.id.textView).text = "Please enter your name!"
+                findViewById<TextView>(R.id.textView).setTextColor(Color.RED)
+            } else {
+                findViewById<TextView>(R.id.textView).text = "Hello $name!"
+            }
+        }
+
     }
 }
